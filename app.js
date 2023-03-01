ed25e59cf17640adf973cbf2d7c203307fdac665
Sam Prestigiacomo <98630238+SamPresto@users.noreply.github.com>
2023-02-28 22:09:18 -0600

    greaterNumber

---
+++ app.js
@@ -0,0 +1,13 @@
+function greaterNumber(x,y) {
+    if(x > y) {
+        return alert(x);
+    }
+    if(y > x){
+        return alert(y);
+    }
+    else {
+        return alert("ERROR...");
+    }
+}
+
+greaterNumber(20, 20);
