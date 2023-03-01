439036c29c08dcbc7376130ca05a49e4a27ecf0a
Sam Prestigiacomo <98630238+SamPresto@users.noreply.github.com>
2023-02-28 22:09:38 -0600

    forLoops

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-function greaterNumber(x,y) {
-    if(x > y) {
-        return alert(x);
+
+// Set numList to any number and the page will alert you, starting from 0 all the way up to whatever number numList is set to
+let numList = 20;
+
+
+// for every number in numList, alert the user if even or odd
+for(let i =0; i <= numList; i++) {
+
+    // if number divided by 2 has no remainder, then it's even
+    if(i % 2 === 0) {
+        alert(`${i} is even`);
     }
-    if(y > x){
-        return alert(y);
-    }
+
+
+    // if number dividen by 2 has a remainder, then it's odd
     else {
-        return alert("ERROR...");
+        alert(`${i} is odd`);
     }
 }
-
-greaterNumber(20, 20);
